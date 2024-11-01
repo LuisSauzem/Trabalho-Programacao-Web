5ad643ba3e0377f01167e42988bdeaf17fd84cfa
luis <luishenrique.mello@hotmail.com>
2024-10-31 21:23:37 -0300

    first commit

--- src/app/componentes/visualizar-tarefas/visualizar-tarefas.component.ts
+++ src/app/componentes/visualizar-tarefas/visualizar-tarefas.component.ts
@@ -6,11 +6,23 @@ import {TarefaService} from "../../app-core/servicos/tarefa-service.service";
   templateUrl: './visualizar-tarefas.component.html',
   styleUrls: ['./visualizar-tarefas.component.css']
 })
+
+
 export class VisualizarTarefasComponent implements OnInit {
+
+  i: number = 0;
 
   constructor(private tarefaService: TarefaService) { }
 
+  addTarefa(){
+    this.tarefaService.addtarefa('Tarefa' + this.i);
+    this.i++;
+  }
+
+
   ngOnInit(): void {
+
+
   }
 
 }
